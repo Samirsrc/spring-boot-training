db0f849436823abe16008ce509585492b9acef2a
samirsrc <41807740+Samirsrc@users.noreply.github.com>
2024-11-12 19:13:10 +0100

    Adding Alien class and some changing in others files

---
+++ Spring1/src/main/java/org/example/Alien.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Alien {
+
+    Alien(){
+        System.out.println("object created");
+    }
+    public void code(){
+        System.out.println("Coding");
+    }
+}
--- Spring1/src/main/java/org/example/App.java
+++ Spring1/src/main/java/org/example/App.java
@@ -1,13 +1,23 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println("before context");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        System.out.println("before get bean and after context");
+        Alien obj1 = (Alien) context.getBean("alien");
+        System.out.println("after get bean");
+        obj1.code();
+
     }
 }
