46cda0c71db557cddc758de987e5c3d1ae43e626
samirsrc <41807740+Samirsrc@users.noreply.github.com>
2025-01-12 16:36:14 +0100

    edit

--- ServletEx/src/main/java/org/example/App.java
+++ ServletEx/src/main/java/org/example/App.java
@@ -8,21 +8,25 @@ import org.apache.catalina.startup.Tomcat;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args ) throws LifecycleException {
+    public static void main(String[] args ) throws LifecycleException {
 
         System.out.println( "Hello World!" );
 
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
+
+        tomcat.setPort(8081);
 
         Context context = tomcat.addContext("",null);
+
         Tomcat.addServlet(context,"HelloServlet",new HelloServlet());
+
         context.addServletMappingDecoded("/hello","HelloServlet",true);
 
         tomcat.start();
         tomcat.getServer().await();
+
 
     }
 }
--- ServletEx/src/main/java/org/example/HelloServlet.java
+++ ServletEx/src/main/java/org/example/HelloServlet.java
@@ -1,12 +1,26 @@
 package org.example;
 
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 //@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-    public void service(HttpServletRequest req, HttpServletResponse res){
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("In service ");
+        PrintWriter out = res.getWriter();
+        out.println("ok je vois");
+        
+
     }
 }
+
+// <dependency>
+//      <groupId>jakarta.servlet</groupId>
+//      <artifactId>jakarta.servlet-api</artifactId>
+//      <version>6.0.0</version>
+//      <scope>provided</scope>
+//    </dependency>
